b73b0197c37186051d47d93419ae011f9aee2d5a
MarkMallari16 <mallariftw01@gmail.com>
2024-07-15 00:05:25 +0800

    remove unnecessary import

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react'
 import { LogInContext } from '../contexts/LogInContext'
-import { Link, NavLink } from 'react-router-dom';
 import SidebarLinks from './SidebarLinks';
 import useSideBar from '../hooks/useSideBar';
 
