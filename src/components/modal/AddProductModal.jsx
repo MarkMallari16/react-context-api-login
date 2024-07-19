76aad94a92b01a509700421ae3771c7ff3140a4c
MarkMallari16 <mallariftw01@gmail.com>
2024-07-19 23:03:03 +0800

    add sorting on price

--- src/components/modal/AddProductModal.jsx
+++ src/components/modal/AddProductModal.jsx
@@ -16,7 +16,11 @@ const AddProductModal = () => {
     return (
         <>
             <Button type="primary" onClick={showModal}>
-                Open Modal
+                Add new product
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                    <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                </svg>
+
             </Button>
             <Modal title="Add new product" open={isModalOpen} onCancel={handleCancel}>
                 <form>
--- src/components/tables/ProductTable.jsx
+++ src/components/tables/ProductTable.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddProductModal from '../modal/AddProductModal'
 import { Button } from 'antd'
 
 const ProductTable = () => {
+    const [products, setProducts] = useState([
+        {
+            id: 1,
+            name: 'The Sliding Mr. Bones (Next Stop, Pottersville)',
+            price: 2000,
+            imageUrl: 'https://img.freepik.com/free-photo/beautiful-elegance-luxury-fashion-green-handbag_1203-7655.jpg?t=st=1721321220~exp=1721324820~hmac=1e47d0f8a91dda7cb0fe4b7558ad5e94b509fd3204ff5c0f59964708cc242b68&w=740'
+        },
+        {
+            id: 2,
+            name: 'Malcolm Lockyer',
+            price: 4000,
+            imageUrl: 'https://img.freepik.com/free-photo/beautiful-elegance-luxury-fashion-green-handbag_1203-7655.jpg?t=st=1721321220~exp=1721324820~hmac=1e47d0f8a91dda7cb0fe4b7558ad5e94b509fd3204ff5c0f59964708cc242b68&w=740'
+        },
+    ]);
+    const [sortOrder, setSortOrder] = useState("asc");
 
+    const sortProducts = () => {
+        const sortedProducts = [...products].sort((a, b) => {
+            return sortOrder === 'asc' ? a.price - b.price : b.price - a.price;
+        })
+
+        setProducts(sortedProducts);
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    }
     return (
 
         <div>
@@ -13,53 +36,31 @@ const ProductTable = () => {
             <table class="table-fixed bg-white w-full rounded-lg shadow-md">
                 <thead className='border-b border-slate-300'>
                     <tr>
-                        <th className=' lg:p-3 text-slate-900'>Product Picture</th>
                         <th className=' lg:p-3 text-slate-900'>Product ID</th>
+                        <th className=' lg:p-3 text-slate-900'>Product Image</th>
                         <th className=' lg:p-3 text-slate-900'>Product Name</th>
-                        <th className=' lg:p-3 text-slate-900'>Rating</th>
-                        <th className=' lg:p-3 text-slate-900'>Price</th>
+                        <th className=' lg:p-3 text-slate-900' onClick={sortProducts}>Price  {sortOrder === 'asc' ? '↑' : '↓'}</th>
                         <th className=' lg:p-3 text-slate-900'>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className='flex justify-center'>
-                            <img src="https://img.freepik.com/free-photo/beautiful-elegance-luxury-fashion-green-handbag_1203-7655.jpg?t=st=1721321220~exp=1721324820~hmac=1e47d0f8a91dda7cb0fe4b7558ad5e94b509fd3204ff5c0f59964708cc242b68&w=740" alt="product" className='w-20' />
-                        </td>
-                        <td className=' p-5 '>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td className=' p-5 text-center'>Malcolm Lockyer</td>
-                        <td className=' p-5 '>
-                            <span className='flex justify-center text-yellow-500'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                </svg>
+                    {products.map((product, _) => (
+                        <tr>
+                            <td className='p-5 text-center'>{product.id}</td>
+                            <td className='flex justify-center'>
+                                <img src={product.imageUrl} className='w-20' />
+                            </td>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                </svg>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                </svg>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                </svg>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                                    <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-                                </svg>
-                            </span>
-
-                        </td>
-                        <td className=' p-5 text-center'>2000</td>
-                        <td>
-                            <span className='flex gap-2 justify-center'>
-                                <Button type='primary' >Edit</Button>
-                                <Button type='primary' danger>Delete</Button>
-                            </span>
-                        </td>
-                    </tr>
+                            <td className=' p-5 text-center'>{product.name}</td>
+                            <td className=' p-5 text-center'>{product.price}</td>
+                            <td>
+                                <span className='flex gap-2 flex-col lg:flex-row justify-center'>
+                                    <Button type='primary' >Edit</Button>
+                                    <Button type='primary' danger>Delete</Button>
+                                </span>
+                            </td>
+                        </tr>
+                    ))}
 
                 </tbody>
             </table>
