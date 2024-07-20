51a2af0a869c0ead34a81a347d1ba08ed39b9542
MarkMallari16 <mallariftw01@gmail.com>
2024-07-21 01:18:26 +0800

    fix some ui in login

--- src/components/LogInComponent.jsx
+++ src/components/LogInComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import LogInBg from '../assets/bg.svg'
+import React, { useContext } from 'react'
+import LogInBg from '../assets/login-bg.svg'
 import { LogInContext } from '../contexts/LogInContext'
 
 
@@ -9,30 +9,31 @@ const LogInComponent = () => {
     const handleEnter = (e) => {
         if (e.key === 'Enter') {
             login(username, password);
-         
+
         }
     };
     return (
         <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 place-items-center '>
-            <div className='hidden  bg-blue-500 h-full w-full lg:flex justify-center items-center'>
+            <div className='hidden  h-full w-full lg:flex justify-center items-center'>
                 <img src={LogInBg} alt="Log In" className='h-96 object-cover ' />
             </div>
 
             <div className='h-full  flex justify-center items-center '>
-                <div className='w-full mx-10 lg:mx-24'>
+                <div className='w-full mx-10 lg:mx-24 border p-12 rounded-lg'>
                     {logoutMessage &&
                         <div className={`py-4 text-center mb-5 rounded-lg border bg-green-500 text-white font-medium transition-opacity duration-1000 ${visible ? 'opacity-100' : 'opacity-0'}`}>
                             <p>{logoutMessage}</p>
                         </div>}
                     <div>
                         <h1 className='text-5xl font-bold text-slate-800'>Hello,  Welcome Back</h1>
+                        <p className='text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ad!</p>
                     </div>
-                    <div className='mt-8'>
-                        <label htmlFor="username" className='text-lg text-slate-800'>Enter Username: </label>
+                    <div className='mt-4'>
+                        <label htmlFor="username" className='text-lg text-slate-700'>Enter Username: </label>
                         <input type="text" id="username" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} onKeyDown={handleEnter} className='mt-1 p-4 w-full border rounded-lg border-slate-300 focus:outline-blue-500' />
                     </div>
                     <div className='mt-4'>
-                        <label htmlFor="username" className='text-lg text-slate-800'>Enter Password: </label>
+                        <label htmlFor="username" className='text-lg text-slate-700'>Enter Password: </label>
                         <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} onKeyDown={handleEnter} className='mt-1 p-4 w-full border rounded-lg border-slate-300 focus:outline-blue-500' />
                     </div>
                     {error && <p className='mt-1 text-red-500'>{error}</p>}
